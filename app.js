ba098f62ebf7292f1417e457fcc384d3524570ea
ccastrom <claudiocastrom7@gmail.com>
2024-08-09 17:24:58 -0400

    Created 'add_numeric_order_to_playlist' function

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const path=require('path');
 
 const directoryPath= 'C:/Users/claud/OneDrive/Desktop/Temas 1';
 
+
+var add_numeric_order_to_playlist=(musicPlayList)=>{
+
+    
+}
+
 fs.readdir(directoryPath, (err,files)=>{
-    var validateDirectory=err?console.log(err):console.log(files)
+    var validateDirectory=err?console.log(err):add_numeric_order_to_playlist(files)
 
    
     
