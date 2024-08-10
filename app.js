7b022298981eb7b633c00dc04f7aeb1e34c4655f
ccastrom <claudiocastrom7@gmail.com>
2024-08-10 19:19:06 -0400

    Add 'order_music_by_numeric_order(musicPlayList)' function to maintain original numeric music order folder using .sort() method with regular expressions

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const fs = require('node:fs');
 const path=require('path');
 
-const directoryPath= 'C:/Users/claud/OneDrive/Desktop/Temas 1';
+const directoryPath= 'C:/Users/claud/OneDrive/Desktop/Temas 1/';
+
+
+fs.readdir(directoryPath, (err,files)=>{
+    var i=0;
+    var validateDirectory=err?console.log(err):add_numeric_order_to_playlist(files);
+});
+
+var order_music_by_numeric_order=(musicPlayList)=>{
+    const arrayMusic=musicPlayList.sort((a,b)=>{
+        const numA = parseInt(a.match(/\d+/)); 
+        const numB = parseInt(b.match(/\d+/)); 
+        
+        return numA - numB;
+
+    })
+
+    return arrayMusic;
+}
 
 
 var add_numeric_order_to_playlist=(musicPlayList)=>{
+
+    var musicPlayListSorted=order_music_by_numeric_order(musicPlayList);
         
-    for (let i = 0; i < musicPlayList.length; i++) {
+    for (let i = 0; i < musicPlayListSorted.length; i++) {
+       
+        console.log(musicPlayListSorted[i])
+       
 
-        var oldPath ='C:/Users/claud/OneDrive/Desktop/Temas 1/'+ musicPlayList[i];
-        var newPath='C:/Users/claud/OneDrive/Desktop/Temas 1/'+ i+")"+musicPlayList[i];
+        var oldPath =directoryPath+ musicPlayListSorted[i];
+        var oldPathSliced=oldPath.slice(42)
+        var newPath=directoryPath+ i+")"+oldPathSliced;
 
-        console.log(newPath)
+        console.log(oldPathSliced)
 
-        // fs.rename(oldPath,newPath,(err)=>{
-        //   if(err){
-        //     console.log(err);
-        //   }else{
-        //     console.log("renamed files")
-        //   }
-        // })
+       
+        fs.rename(oldPath,newPath,(err)=>{
+          if(err){
+            console.log(err);
+          }else{
+            console.log("renamed files")
+          }
+        })
 
        
        
@@ -28,8 +53,3 @@ var add_numeric_order_to_playlist=(musicPlayList)=>{
     
 }
 
-fs.readdir(directoryPath, (err,files)=>{
-    var i=0;
-    var validateDirectory=err?console.log(err):add_numeric_order_to_playlist(files);
-
-});
