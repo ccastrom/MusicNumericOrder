6b6d6cbc26e56c99a6e987d2e7d0d1406fa9473e
ccastrom <claudiocastrom7@gmail.com>
2024-08-10 16:16:37 -0400

    Added a for loop to iterate and add a numeric order to the songs

--- app.js
+++ app.js
@@ -5,14 +5,31 @@ const directoryPath= 'C:/Users/claud/OneDrive/Desktop/Temas 1';
 
 
 var add_numeric_order_to_playlist=(musicPlayList)=>{
+        
+    for (let i = 0; i < musicPlayList.length; i++) {
+
+        var oldPath ='C:/Users/claud/OneDrive/Desktop/Temas 1/'+ musicPlayList[i];
+        var newPath='C:/Users/claud/OneDrive/Desktop/Temas 1/'+ i+")"+musicPlayList[i];
+
+        console.log(newPath)
+
+        // fs.rename(oldPath,newPath,(err)=>{
+        //   if(err){
+        //     console.log(err);
+        //   }else{
+        //     console.log("renamed files")
+        //   }
+        // })
+
+       
+       
+    }
 
     
 }
 
 fs.readdir(directoryPath, (err,files)=>{
-    var validateDirectory=err?console.log(err):add_numeric_order_to_playlist(files)
-
-   
-    
+    var i=0;
+    var validateDirectory=err?console.log(err):add_numeric_order_to_playlist(files);
 
 });
